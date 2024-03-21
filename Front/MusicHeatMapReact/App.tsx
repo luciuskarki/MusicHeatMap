b0a0a5f8ee64bef988c6cdb6be348ef5137f7e39
Justin Ecarma <just.ecarma@gmail.com>
2024-03-20 20:07:03 -0400

    oops added another change to make it work

--- Front/MusicHeatMapReact/App.tsx
+++ Front/MusicHeatMapReact/App.tsx
@@ -107,12 +107,12 @@ function HomeScreen() {
             <MapView
                     provider={PROVIDER_GOOGLE}
                     style={styles.map}
-                    initialRegion={
+                    initialRegion={{
                       latitude: 37.78825,
                       longitude: -122.4324,
                       latitudeDelta: 0.0922,
                       longitudeDelta: 0.0421,
-                    }
+                    }}
                   >
             </MapView>
 

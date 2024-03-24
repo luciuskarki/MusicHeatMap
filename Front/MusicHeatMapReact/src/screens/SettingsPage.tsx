e6b774aafb1ad5d51f0d888fecf5ec5bc1cfcaa8
Solarific <roflwotnoob@live.com>
2024-03-24 19:33:02 -0400

    Refactored Settings Page

---
+++ Front/MusicHeatMapReact/src/screens/SettingsPage.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import {
+    SafeAreaView,
+    ScrollView,
+    Image,
+    StatusBar,
+    TextInput,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Button,
+    Alert,
+    TouchableOpacity
+  } from 'react-native';
+
+
+  function SettingsScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+  
+  
+  export default SettingsScreen;

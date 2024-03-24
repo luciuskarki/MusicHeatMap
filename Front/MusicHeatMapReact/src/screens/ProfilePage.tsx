4d9c65589163e0dcdcaa537f82505c69159f895c
Solarific <roflwotnoob@live.com>
2024-03-24 19:32:36 -0400

    Refactored Profile Page

---
+++ Front/MusicHeatMapReact/src/screens/ProfilePage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {
+    SafeAreaView,
+    ScrollView,
+    Image,
+    StatusBar,
+    TextInput,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Button,
+    Alert,
+    TouchableOpacity
+  } from 'react-native';
+
+  function ProfileScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Customize Your Profile!</Text>
+      </View>
+    );
+  }
+  
+  export default ProfileScreen;
